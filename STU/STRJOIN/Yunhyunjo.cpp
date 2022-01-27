79a28e1a53ba4ec4a65753b9f1649edb283b3ac8
Yunhyunjo <gotqlc147@likelion.org>
2022-01-27 20:28:50 +0900

    STRJOIN 4ms

---
+++ STU/STRJOIN/Yunhyunjo.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int strjoin(priority_queue <int> pq){
+
+    int res = 0;
+
+    while (pq.size() != 1) {
+        int a = -pq.top();
+        pq.pop();
+        int b = -pq.top();
+        pq.pop();
+        res = res + a + b;
+        pq.push(-(a + b));
+    }
+    
+    return res;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, num;
+
+    cin >> n;
+    
+    while (n--) {
+        cin >> num;
+        priority_queue <int> pq;
+        int a;
+        for (int i = 0; i < num; i++) {
+            cin >> a;
+            pq.push(-a);
+        }
+        cout << strjoin(pq) << "\n";
+    }
+
+    return 0;
+}
